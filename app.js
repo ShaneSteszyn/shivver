e7991bce4a6f14c5e9db9a5cc08396ec02549a8c
Trevor Starick <trevor.starick@gmail.com>
2014-06-17 00:15:49 -0400

    New method of serving video

--- app.js
+++ app.js
@@ -108,8 +108,40 @@ app.get('/videos/:video', function(req, res) {
   var video = decodeURI(req.params.video);
   var game = video.split('.')[0];
   game = game.substring(0, game.length - 3);
+
   var path = _path.resolve(spd + '/' + game + '/' + video);
-  res.sendfile(path);
+  var stat = fs.statSync(path);
+  var total = stat.size;
+  if (req.headers.range) {
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : total - 1;
+    var chunksize = (end - start) + 1;
+    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+
+    var file = fs.createReadStream(path, {
+      start: start,
+      end: end
+    });
+    res.writeHead(206, {
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4'
+    });
+    file.pipe(res);
+  } else {
+    console.log('ALL: ' + total);
+    res.writeHead(200, {
+      'Content-Length': total,
+      'Content-Type': 'video/mp4'
+    });
+    fs.createReadStream(path).pipe(res);
+  }
 });
 
 http.listen(3000);
